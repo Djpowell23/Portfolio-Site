49f352ff800e6c077ba338824f7aff63c7c2bde9
Devin Powell <dpowel11@kent.edu>
2019-06-23 12:03:32 -0600

    added express burger assignment to portfolio

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -210,7 +210,7 @@ $('#project-list').on('click', '#liri', function () {
 // Show Bamazon App Video
 $('#project-list').on('click', '#bamazon', function () {
     // Create variable with new video
-    currentVideo = $('<iframe width="560" height="315" src="https://youtu.be/v7Yat-rtLGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+    currentVideo = $('<iframe width="560" height="315" src="https://www.youtube.com/embed/v7Yat-rtLGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
 
     // Images to take video spot until I can record them
     //  $('#video-player').append(
@@ -259,6 +259,42 @@ $('#project-list').on('click', '#friendfinder', function () {
     siteBtn.addClass('detail-btn');
     // Add onclick event
     siteBtn.attr('onClick', 'window.open("https://vast-coast-93774.herokuapp.com/")')
+    // Add text to button
+    siteBtn.text('Link to Site');
+
+    // Append Buttons to HTML
+    $('#project-details').append(codeBtn);
+    $('#project-details').append(siteBtn);
+    // Append Video to video player id
+    $('#video-player').append(currentVideo);
+});
+
+// Show Burger App Image
+$('#project-list').on('click', '#expressburger', function () {
+    // Create variable with new video
+    // currentVideo = $('<iframe width="560" height="315" src="https://youtu.be/v7Yat-rtLGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+
+    // Images to take video spot until I can record them
+    $('#video-player').append(
+        '<img class="placeholder" style="width:620px; height:400px;" ' +
+        'src="assets/images/expressburger.jpg"' + 'alt="burger-image">' + '</img>'
+    );
+
+    // Create variable to store Link-To-Code Button
+    var codeBtn = $('<button>');
+    // Add classes to codeBtn
+    codeBtn.addClass('detail-btn');
+    // Add onclick event
+    codeBtn.attr('onClick', 'window.open("https://github.com/Djpowell23/burger")');
+    // Add text to Button
+    codeBtn.text('Link to Code');
+
+    // Create variable to store Link-To-Website Button
+    var siteBtn = $('<button>');
+    // Add classes to siteBtn
+    siteBtn.addClass('detail-btn');
+    // Add onclick event
+    siteBtn.attr('onClick', 'window.open("https://damp-lake-65910.herokuapp.com/")')
     // Add text to button
     siteBtn.text('Link to Site');
 
